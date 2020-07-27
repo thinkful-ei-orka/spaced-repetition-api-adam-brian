ba304157945b1e56d13236877ac673586a135f04
Adam Brown <adam.edward.brown@gmail.com>
2020-07-27 13:59:04 -0400

    cypress tests running

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,41 +1,47 @@
 BEGIN;
 
-TRUNCATE
+  TRUNCATE
   "word",
   "language",
   "user";
 
-INSERT INTO "user" ("id", "username", "name", "password")
-VALUES
-  (
-    1,
-    'admin',
-    'Dunder Mifflin Admin',
-    -- password = "pass"
-    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
-  );
+  INSERT INTO "user"
+    ("id", "username", "name", "password")
+  VALUES
+    (
+      1,
+      'admin',
+      'Admin',
+      -- password = "pass"
+      '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+    );
 
-INSERT INTO "language" ("id", "name", "user_id")
-VALUES
-  (1, 'French', 1);
+  INSERT INTO "language"
+    ("id", "name", "user_id")
+  VALUES
+    (1, 'Spanish', 1);
 
-INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
-VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  INSERT INTO "word"
+    ("id", "language_id", "original", "translation", "next")
+  VALUES
+    (1, 1, 'Tengo hambre', 'I''m hungry', 2),
+    (2, 1, 'hola', 'hello', 3),
+    (3, 1, 'casa', 'house', 4),
+    (4, 1, 'cabeza', 'head', 5),
+    (5, 1, 'fácil', 'easy', 6),
+    (6, 1, '¿Cómo estás?', 'How are you?', 7),
+    (7, 1, 'perro', 'dog', 8),
+    (8, 1, 'gato', 'cat', null);
 
-UPDATE "language" SET head = 1 WHERE id = 1;
+  UPDATE "language" SET head = 1 WHERE id = 1;
 
--- because we explicitly set the id fields
--- update the sequencer for future automatic id setting
-SELECT setval('word_id_seq', (SELECT MAX(id) from "word"));
-SELECT setval('language_id_seq', (SELECT MAX(id) from "language"));
-SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+  -- because we explicitly set the id fields
+  -- update the sequencer for future automatic id setting
+  SELECT setval('word_id_seq', (SELECT MAX(id)
+    from "word"));
+  SELECT setval('language_id_seq', (SELECT MAX(id)
+    from "language"));
+  SELECT setval('user_id_seq', (SELECT MAX(id)
+    from "user"));
 
-COMMIT;
+  COMMIT
--- src/user/user-service.js
+++ src/user/user-service.js
@@ -46,7 +46,7 @@ const UserService = {
       const [languageId] = await trx
         .into('language')
         .insert([
-          { name: 'French', user_id },
+          { name: 'Spanish', user_id },
         ], ['id'])
 
       // when inserting words,
@@ -58,14 +58,14 @@ const UserService = {
         .first()
 
       const languageWords = [
-        ['entraine toi', 'practice', 2],
-        ['bonjour', 'hello', 3],
-        ['maison', 'house', 4],
-        ['développeur', 'developer', 5],
-        ['traduire', 'translate', 6],
-        ['incroyable', 'amazing', 7],
-        ['chien', 'dog', 8],
-        ['chat', 'cat', null],
+        ['Tengo hambre', `Im hungry`, 2],
+        ['hola', 'hello', 3],
+        ['casa', 'house', 4],
+        ['cabeza', 'head', 5],
+        ['fácil', 'easy', 6],
+        ['¿Cómo estás?', 'How are you?', 7],
+        ['perro', 'dog', 8],
+        ['gato', 'cat', null]
       ]
 
       const [languageHeadId] = await trx
