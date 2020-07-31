2b36c15097b48aef59e61defc62283fcd03539f7
Adam Brown <adam.edward.brown@gmail.com>
2020-07-31 13:46:44 -0400

    predeploy 2

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
+  "connectionString": (process.env.Node_ENV === 'test') ? process.env.Test_DATABASE_URL : process.env.DATABASE_URL,
   "host": process.env.MIGRATION_DB_HOST,
   "port": process.env.MIGRATION_DB_PORT,
   "database": process.env.NODE_ENV === 'development' ? process.env.TEST_MIGRATION_DB_NAME : process.env.MIGRATION_DB_NAME,
